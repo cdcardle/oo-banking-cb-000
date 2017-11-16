75cb9ffd37acfb97f8e0a1b07233e47c0395f8e8
Chris Cardle <cdcardle@gmail.com>
2017-11-16 16:31:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -35,4 +35,11 @@ class Transfer
       "Transaction rejected. Please check your account balance."
       self.status = "rejected"
   end
+
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance += self.amount
+      self.receiver.balance += self.amount
+      self.status = "reversed"
+  end
 end
